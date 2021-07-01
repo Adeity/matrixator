62b225227668d850e3930b825712f47bef5c598d
Chiril Babaev <babaechi@fel.cvut.cz>
2021-07-01 17:54:47 +0200

    new tests for future parsing

--- spec/calculations/parser.mjs
+++ spec/calculations/parser.mjs
@@ -10,7 +10,73 @@ describe("Element parser", function() {
         expect(actualResult).toEqual(expectedResult)
     });
 
-    it("with input 10 * 10 should return 10 * 10", function () {
+    it("parseSingleElementToString should return ", function () {
+        let inputElement = ""
 
+        let expectedResult = ""
+
+        let actualResult = parseSingleElementToString(inputElement)
+
+        expect(actualResult).toEqual(expectedResult)
+    });
+
+    it("parseSingleElementToString should return '3a + 3b' with input '3   a + b3' ", function () {
+        let inputElement = "3   a + b3"
+
+        let expectedResult = "3a + 3b"
+
+        let actualResult = parseSingleElementToString(inputElement)
+
+        expect(actualResult).toEqual(expectedResult)
+    });
+
+    it("parseSingleElementToString should return '5a + 3b + 3c' with input '3   a + b3 + c + c + c + 2a    ' ", function () {
+        let inputElement = "3   a + b3 + c + c + c + 2a     "
+
+        let expectedResult = "5a + 3b + 3c"
+
+        let actualResult = parseSingleElementToString(inputElement)
+
+        expect(actualResult).toEqual(expectedResult)
+    });
+
+    it("parseSingleElementToString should return '20ab' with input '4a * 5b' ", function () {
+        let inputElement = "3   a + b3 + c + c + c + 2a     "
+
+        let expectedResult = "5a + 3b + 3c"
+
+        let actualResult = parseSingleElementToString(inputElement)
+
+        expect(actualResult).toEqual(expectedResult)
+    });
+
+    it("parseSingleElementToString should return '12a / b' with input '3a / b4' ", function () {
+        let inputElement = "3   a + b3 + c + c + c + 2a     "
+
+        let expectedResult = "5a + 3b + 3c"
+
+        let actualResult = parseSingleElementToString(inputElement)
+
+        expect(actualResult).toEqual(expectedResult)
+    });
+
+    it("parseSingleElementToString should return '3ab / 4' with input '3a / 4b' ", function () {
+        let inputElement = "3   a + b3 + c + c + c + 2a     "
+
+        let expectedResult = "5a + 3b + 3c"
+
+        let actualResult = parseSingleElementToString(inputElement)
+
+        expect(actualResult).toEqual(expectedResult)
+    });
+
+    it("parseSingleElementToString should return '3a / (4b)' with input '3a / (4b)' ", function () {
+        let inputElement = "3   a + b3 + c + c + c + 2a     "
+
+        let expectedResult = "5a + 3b + 3c"
+
+        let actualResult = parseSingleElementToString(inputElement)
+
+        expect(actualResult).toEqual(expectedResult)
     });
 });
